b584d7873219102ea7b973216f8ae128eb8f7f68
glalidis <glalidis10@gmail.com>
2025-06-24 16:38:15 +0300

    fix backend for render deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 app.use(cors());
 app.use(express.json());
